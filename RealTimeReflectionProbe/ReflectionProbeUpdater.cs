810087f655efb4f111823362270829893404f456
DESKTOP-LQCU3R3\m_e_m <christian.c@idiomaplus.com>
2022-02-24 14:24:16 -0700

    UpdateFiles

---
+++ RealTimeReflectionProbe/ReflectionProbeUpdater.cs
@@ -0,0 +1,30 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ReflectionProbeUpdater : UdonSharpBehaviour
+{
+    [Tooltip("Update frecuency on seconds. Default 0.3 seconds")]
+    public float syncFrequency = 0.3f;
+
+    private void Update()
+    {
+        SyncReflection();
+    }
+
+    float _lastSyncTime;
+
+    void SyncReflection()
+    {
+        float timeSinceStartup = Time.realtimeSinceStartup;
+
+        if (timeSinceStartup - _lastSyncTime > syncFrequency)
+        {
+            _lastSyncTime = timeSinceStartup;
+            this.gameObject.GetComponent<ReflectionProbe>().RenderProbe();
+        }
+    }
+
+}
